15a25ec387d262caed097c3020895bf5a1322088
Hady Tawfik <hady.tawfik1999@gmail.com>
2024-12-04 17:51:26 +0200

    Removing the syncCart route

--- routes/cart.js
+++ routes/cart.js
@@ -1,5 +1,4 @@
 import { Router } from 'express';
-import { authenticateUser } from '../middlewares/full-auth.js';
 import * as controllers from '../controllers/cart.js';
 
 const router = Router();
@@ -9,8 +8,6 @@ router
 	.get(controllers.getCart)
 	.post(controllers.addItemToCart)
 	.delete(controllers.deleteCart);
-
-router.get('/sync', authenticateUser, controllers.syncCart);
 
 router.post('/:itemId/reduce-one', controllers.reduceByone);
 router.post('/:itemId/increase-one', controllers.increaseByone);
